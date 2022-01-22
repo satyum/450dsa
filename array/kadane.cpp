af2ed5b99b03cb9aebf9200cb1d7de4721022b0a
satyum <kingsatyam7@gmail.com>
2022-01-22 15:36:48 +0000

    Create kadane.cpp

---
+++ array/kadane.cpp
@@ -0,0 +1,35 @@
+//find Largest sum contiguous Subarray
+    #include<bits/stdc++.h> 
+    using namespace std;
+    typedef long long ll;
+
+    int main()
+    {
+    	int n;
+    	cin>>n;
+    	vector<ll>val(n);
+    	for(int i=0;i<n;++i)
+    	{
+    		cin>>val[i];
+    	}
+    	ll best=-1e18;
+    	ll past=0;
+     
+    	for(int i=0;i<n;++i)
+    	{
+    		if(past+val[i]>=val[i])
+    		{
+    			past+=val[i];
+    		}
+    		else
+    		{
+    			past=val[i];
+     
+    		}
+    	best=max(best,past);
+    	}
+    	cout<<best;
+     
+    }
+     
+
