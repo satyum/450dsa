7557b556a051f7eacd8721338cca97decca5c296
satyum <kingsatyam7@gmail.com>
2022-01-23 06:35:23 +0000

    Update palindromicstring.cpp

--- strings/palindromicstring.cpp
+++ strings/palindromicstring.cpp
@@ -26,3 +26,33 @@ int main()
 	}
 	return 0;
 }
+
+//different
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	
+	string s;
+	cin>>s;
+
+	int l=0;
+	int r=s.size()-1;
+	while(l<r)
+	{
+		if(s[l]!=s[r])
+		{
+			 cout<<"not palindrome" ;
+			break;
+		}	
+		else{
+			cout<<"palindrome";
+			}
+		l++;
+		r--;
+	
+	}
+	
+	return 0;
+}
